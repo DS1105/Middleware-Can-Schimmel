ee9714a3eb6b551aa6f3539be743d3fab96cbfd5
Sera <02case1bwi@hft-stuttgart.de>
2025-01-05 14:29:34 +0100

    Änderunge

--- shopping-server/server.js
+++ shopping-server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = 5001;
 
 app.use(cors());
 app.use(bodyParser.json());
